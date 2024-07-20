90f163b266230f563709ae8168018d0b21d33a31
rtTAP <tammyapowell@hotmail.com>
2024-07-20 14:23:42 +0800

    TAP-corrected assign depth colours

--- Leaflet-Visualisation/static/js/logic.js
+++ Leaflet-Visualisation/static/js/logic.js
@@ -5,8 +5,8 @@ let url =
 // Create the map
 console.log(1);
 let myMap = L.map("map", {
-  center: [10, 90],
-  zoom: 2,
+  center: [39.8283, -98.5795],
+  zoom: 4,
 });
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -17,47 +17,42 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 // Assign depth colours based on the data
 function getDepthColour(depth) {
   if (depth >= -10 && depth <= 10) {
-    colour = "#FFCC9A";
+    return "#FFCC9A";
   } else if (depth > 10 && depth <= 30) {
-    colour = "#EC988E";
+    return "#EC988E";
   } else if (depth > 30 && depth <= 50) {
-    colour = "#DC828E";
+    return "#DC828E";
   } else if (depth > 50 && depth <= 70) {
-    colour = "#C76B8F";
+    return "#C76B8F";
   } else if (depth > 70 && depth <= 90) {
-    colour = "#8E5A91";
+    return "#8E5A91";
   } else {
-    colour = "#634C8D";
+    return "#634C8D";
   }
-
-  return colour;
 }
 
 // Read the data
 d3.json(url).then(function (data) {
-  L.geoJson(data);
-  for (let i = 0; i < data.features.length; i++) {
-    var long = data.features[i].geometry.coordinates[0];
-    var lat = data.features[i].geometry.coordinates[1];
-    var depth = data.features[i].geometry.coordinates[2];
-    var magnitude = data.features[i].properties.mag;
-    var place = data.features[i].properties.place;
-    var tsunami = Boolean(data.features[i].properties.tsunami);
-    var radius_eq = Math.abs(magnitude) * 50000;
-    var depthColour = getDepthColour(depth);
+  L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+      var depth = feature.geometry.coordinates[2];
+      var magnitude = feature.properties.mag;
+      var place = feature.properties.place;
+      var tsunami = Boolean(feature.properties.tsunami);
+      var radius_eq = Math.abs(magnitude) * 50000;
+      var depthColour = getDepthColour(depth);
 
-    // Add marker
-    L.circle([lat, long], {
-      radius: radius_eq,
-      color: depth,
-      fillColor: depthColour,
-      fillOpacity: 0.6,
-    })
-      .bindPopup(
+      return L.circle(latlng, {
+        radius: radius_eq,
+        color: depthColour,
+        fillColor: depthColour,
+        fillOpacity: 0.6,
+      }).bindPopup(
         `<h3>${place} </h3> Depth: ${depth} km<br>Magnitude: ${magnitude} <br> Tsunami: ${tsunami}`
-      )
-      .addTo(myMap);
-  }
+      );
+    },
+  }).addTo(myMap);
+
 
   // Add legend
   let limits = ["-10 - 10", "10 - 30", "30 - 50", "50 - 70", "70 - 90", "90+"];
@@ -70,38 +65,25 @@ d3.json(url).then(function (data) {
     "#634C8D",
   ];
 
-
-  let legend = L.control({position: "bottomright" });
-  //console.log(1);
+  let legend = L.control({ position: "bottomright" });
   legend.onAdd = function () {
     let div = L.DomUtil.create("div", "info legend");
 
     limits.forEach(function (limit, index) {
-      let box = '<div id="square_legend" style="background-color: ' +
-          colours[index] +
-          '">'+ '<div id="text_legend">' + limit + '</div>' + '</div>'
-      ;
-      
+      let box =
+        '<div id="square_legend" style="background-color: ' +
+        colours[index] +
+        '">' +
+        '<div id="text_legend">' +
+        limit +
+        "</div>" +
+        "</div>";
       div.innerHTML += "<div class='container'>" + box + "</div>";
     });
 
-    
     return div;
   };
 
   legend.addTo(myMap);
 });
 
-//  let legend = L.control({ position: "bottomright" });
-//  console.log(1);
-//  legend.onAdd = function () {
-//    let div = L.DomUtil.create("div", "info legend");
-//    let limits = depth;
-//    let colors = depth;
-//    let labels = [];
-//  };
-//  let legendInfo = "<h1>Earthquake<br />(ages 6-17)</h1>";
-
-// div.innerHTML = legendInfo;
-//  legend.addTo(myMap);
-//});
